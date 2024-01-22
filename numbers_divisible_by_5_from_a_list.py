60e1f73d4db042f85f4ca061ce1f59698cc277ea
unknown <centinaraphael0157@gmail.com>
2024-01-22 23:53:51 +0800

    testing while

--- numbers_divisible_by_5_from_a_list.py
+++ numbers_divisible_by_5_from_a_list.py
@@ -1,11 +1,11 @@
 #Iterate the given list of numbers and print only those numbers which are divisible by 5
 number_list = list()
-number_list.append('10')
-number_list.append('20')
-number_list.append('33')
-number_list.append('46')
-number_list.append('55')
+number_list.append(10)
+number_list.append(20)
+number_list.append(33)
+number_list.append(46)
+number_list.append(55)
 print(number_list)
 
-if number_list % 5 == 0:
+while number_list == /5:
     print (number_list)
