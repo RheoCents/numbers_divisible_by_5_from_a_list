ad01c24830acbe74db8566c910b2d27dcb5dbb69
unknown <centinaraphael0157@gmail.com>
2024-01-22 23:41:18 +0800

    testing list

---
+++ numbers_divisible_by_5_from_a_list.py
@@ -0,0 +1,8 @@
+#Iterate the given list of numbers and print only those numbers which are divisible by 5
+number_list = list()
+number_list.append('10')
+number_list.append('20')
+number_list.append('33')
+number_list.append('46')
+number_list.append('55')
+print(number_list)
